98a6c289054a40af8e16da292d56f022d5cbd0de
Colli456 <cjclarke14y@gmail.com>
2024-02-11 05:57:12 -0500

    Test 3 Complete

--- tests/js-basic-03.test.js
+++ tests/js-basic-03.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef-init */
 /* eslint-disable prefer-const */
 /* eslint-disable no-undef */
@@ -27,6 +28,8 @@ TODO: to the boolean value true.
 */
 
 // * Add your code BELOW here
+
+let isSeptember = true;
 
 // * Don't mess with the test code below here!
 
@@ -58,6 +61,8 @@ TODO: Do not use the reserved word false.
 
 // * Add your code BELOW here
 
+let isFalsy = null;
+
 // * Don't mess with the test code below here!
 
 test('isFalsy', () => {
@@ -83,6 +88,8 @@ TODO: Do not use the reserved word true.
 
 // * Add your code BELOW here
 
+let isTruthy = 1;
+
 // * Don't mess with the test code below here!
 
 test('isTruthy', () => {
@@ -105,6 +112,8 @@ TODO: unassigned.
 */
 
 // * Add your code BELOW here
+
+let notAssigned;
 
 // * Don't mess with the test code below here!
 
@@ -131,6 +140,8 @@ TODO: null.
 
 // * Add your code BELOW here
 
+let bloop = null;
+
 // * Don't mess with the test code below here!
 
 test('bloop', () => {
@@ -153,6 +164,8 @@ TODO: the boolean value of !isSeptember.
 
 // * Add your code BELOW here
 
+let notSeptember = !isSeptember;
+
 // * Don't mess with the test code below here!
 
 test('notSeptember', () => {
@@ -173,6 +186,9 @@ TODO: the boolean value of !!user.
 */
 
 // * Add your code BELOW here
+
+let user = '';
+let isLoggedIn = !!user;
 
 // * Don't mess with the test code below here!
 
